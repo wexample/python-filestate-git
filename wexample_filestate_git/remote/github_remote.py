fa1c080a7ce947e0489c8d03b7ea2faeaa950dda
weeger <romain.weeger@gmail.com>
2025-01-09 21:10:36 +0100

    Rework remote

--- wexample_filestate_git/remote/github_remote.py
+++ wexample_filestate_git/remote/github_remote.py
@@ -1,26 +1,33 @@
 import re
-from typing import Dict, List, Optional
+from typing import Dict, List
 from pydantic import Field
 
 from .abstract_remote import AbstractRemote
 
 
+GITHUB_ENV_KEY_TOKEN: str = "GITHUB_API_TOKEN"
+GITHUB_API_VERSION: str = "application/vnd.github.v3+json"
+GITHUB_DEFAULT_URL: str = "https://api.github.com"
+
+
 class GithubRemote(AbstractRemote):
     base_url: str = Field(
-        default="https://api.github.com",
+        default=GITHUB_DEFAULT_URL,
         description="GitHub API base URL"
     )
 
     def model_post_init(self, *args, **kwargs):
         super().model_post_init(*args, **kwargs)
-        if self.api_keys.get("token"):
+        if self.api_keys.get(GITHUB_ENV_KEY_TOKEN):
             self.default_headers.update({
-                "Authorization": f"token {self.api_keys['token']}",
-                "Accept": "application/vnd.github.v3+json"
+                "Authorization": f"token {self.api_keys[GITHUB_ENV_KEY_TOKEN]}",
+                "Accept": GITHUB_API_VERSION
             })
 
     def get_expected_env_keys(self) -> List[str]:
-        return ["token"]
+        return [
+            GITHUB_ENV_KEY_TOKEN,
+        ]
 
     def create_repository(self, name: str, description: str = "", private: bool = False) -> Dict:
         response = self.make_request(
@@ -39,7 +46,6 @@ class GithubRemote(AbstractRemote):
         github_patterns = [
             r"git@github\.com:",
             r"https://github\.com/",
-            r"git://github\.com/",
-            r"gitlab"
+            r"git://github\.com/"
         ]
         return any(re.search(pattern, remote_url) for pattern in github_patterns)
--- wexample_filestate_git/remote/gitlab_remote.py
+++ wexample_filestate_git/remote/gitlab_remote.py
@@ -1,25 +1,31 @@
 import re
-from typing import Dict, List, Optional
+from typing import Dict, List
 from pydantic import Field
 
 from .abstract_remote import AbstractRemote
 
 
+GITLAB_ENV_KEY_TOKEN: str = "GITLAB_API_TOKEN"
+GITLAB_DEFAULT_URL: str = "https://gitlab.com/api/v4"
+
+
 class GitlabRemote(AbstractRemote):
     base_url: str = Field(
-        default="https://gitlab.com/api/v4",
+        default=GITLAB_DEFAULT_URL,
         description="GitLab API base URL"
     )
 
     def model_post_init(self, *args, **kwargs):
         super().model_post_init(*args, **kwargs)
-        if self.api_keys.get("token"):
+        if self.api_keys.get(GITLAB_ENV_KEY_TOKEN):
             self.default_headers.update({
-                "PRIVATE-TOKEN": self.api_keys["token"]
+                "PRIVATE-TOKEN": self.api_keys[GITLAB_ENV_KEY_TOKEN]
             })
 
     def get_expected_env_keys(self) -> List[str]:
-        return ["token"]
+        return [
+            GITLAB_ENV_KEY_TOKEN,
+        ]
 
     def create_repository(self, name: str, description: str = "", private: bool = False) -> Dict:
         response = self.make_request(
@@ -38,6 +44,7 @@ class GitlabRemote(AbstractRemote):
         gitlab_patterns = [
             r"git@gitlab\.com:",
             r"https://gitlab\.com/",
-            r"git://gitlab\.com/"
+            r"git://gitlab\.com/",
+            r"://gitlab",
         ]
         return any(re.search(pattern, remote_url) for pattern in gitlab_patterns)
