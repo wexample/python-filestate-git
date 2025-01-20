8347d8bbeb5d239b6aea4f0fecd094813a5b2bf0
weeger <romain.weeger@gmail.com>
2025-01-20 18:53:26 +0100

    Move polyfill

--- wexample_filestate_git/operations_provider/git_operations_provider.py
+++ wexample_filestate_git/operations_provider/git_operations_provider.py
@@ -1,7 +1,7 @@
 from typing import List, Type, TYPE_CHECKING
 
 from wexample_filestate.operations_provider.abstract_operations_provider import AbstractOperationsProvider
-from wexample_helpers.helpers.polyfill import polyfill_import
+from wexample_helpers.helpers.polyfill import polyfill_register_global
 
 if TYPE_CHECKING:
     from wexample_filestate.operation.abstract_operation import AbstractOperation
@@ -15,7 +15,7 @@ class GitOperationsProvider(AbstractOperationsProvider):
         from wexample_filestate_git.operation.git_remote_create_operation import GitRemoteCreateOperation
         from wexample_filestate.const.state_items import TargetFileOrDirectory
 
-        polyfill_import(TargetFileOrDirectory)
+        polyfill_register_global(TargetFileOrDirectory)
 
         GitRemoteAddOperation.model_rebuild()
         GitRemoteCreateOperation.model_rebuild()
