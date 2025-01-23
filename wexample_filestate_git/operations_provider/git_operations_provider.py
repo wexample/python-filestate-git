24d1eb917201ea6bcfbbc8fc2eac961cbcfb1112
weeger <romain.weeger@gmail.com>
2025-01-23 09:15:57 +0100

    Minor fix

--- wexample_filestate_git/operations_provider/git_operations_provider.py
+++ wexample_filestate_git/operations_provider/git_operations_provider.py
@@ -1,7 +1,6 @@
 from typing import List, Type, TYPE_CHECKING
 
 from wexample_filestate.operations_provider.abstract_operations_provider import AbstractOperationsProvider
-from wexample_helpers.helpers.polyfill import polyfill_register_global
 
 if TYPE_CHECKING:
     from wexample_filestate.operation.abstract_operation import AbstractOperation
@@ -14,8 +13,9 @@ class GitOperationsProvider(AbstractOperationsProvider):
         from wexample_filestate_git.operation.git_remote_add_operation import GitRemoteAddOperation
         from wexample_filestate_git.operation.git_remote_create_operation import GitRemoteCreateOperation
         from wexample_filestate.const.state_items import TargetFileOrDirectory
+        from wexample_helpers.helpers.polyfill import polyfill_import
 
-        polyfill_register_global(TargetFileOrDirectory)
+        polyfill_import(TargetFileOrDirectory)
 
         GitRemoteAddOperation.model_rebuild()
         GitRemoteCreateOperation.model_rebuild()
