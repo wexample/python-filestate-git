f52b455e027fa977092fbb2cde00b11bb8e53e01
weeger <romain.weeger@gmail.com>
2025-01-25 21:03:52 +0100

    Remote as config option, draft

--- wexample_filestate_git/config_option/remote_config_option.py
+++ wexample_filestate_git/config_option/remote_config_option.py
@@ -1,15 +1,5 @@
-from types import UnionType
-from typing import Type, Any, List
-
-# TODO from wexample_config.config_option.abstract_list_config_option import AbstractListConfigOption
-from wexample_config.config_option.config_option import ConfigOption
+from wexample_config.config_option.abstract_list_config_option import AbstractListConfigOption
 
 
-class RemoteConfigOption(ConfigOption):
-    @staticmethod
-    def get_raw_value_allowed_type() -> Any:
-        return List[dict[str, Any]]
-
-    @staticmethod
-    def get_allowed_types() -> Type | UnionType:
-        return List[dict[str, Any]]
+class RemoteConfigOption(AbstractListConfigOption):
+    pass
