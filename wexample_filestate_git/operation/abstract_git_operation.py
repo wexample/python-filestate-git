eced3299df21898ce6f5d010816f20fbdf0c49ef
weeger <romain.weeger@gmail.com>
2025-08-22 11:47:29 +0200

    Remove futures

--- wexample_filestate_git/operation/abstract_git_operation.py
+++ wexample_filestate_git/operation/abstract_git_operation.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from abc import ABC
 
 from wexample_filestate.operation.abstract_operation import AbstractOperation
--- wexample_filestate_git/operation/git_remote_add_operation.py
+++ wexample_filestate_git/operation/git_remote_add_operation.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from pathlib import PosixPath
 from typing import TYPE_CHECKING, Any, cast
 
